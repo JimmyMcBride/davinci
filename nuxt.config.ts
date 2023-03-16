65bf78fd1067ef19e3a9fa07881f5825a562b744
Jimmy McBride <jimmy@ravebizz.com>
2023-03-15 21:37:15 -0400

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "DaVinci",
+      meta: [
+        {
+          name: "description",
+          content: "OpenAI's GPT-3 powered project management tool",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY,
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet aperiam delectus in ipsum iure
+      laboriosam necessitatibus quisquam repellat tempore.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet aperiam delectus in ipsum iure
+      laboriosam necessitatibus quisquam repellat tempore.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: "Home",
+})
+</script>
+
+<style scoped>
+h1 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
