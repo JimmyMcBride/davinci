a5cdf7999225ff8e6e87f82690922a4986b522ef
Jimmy McBride <jimmy@ravebizz.com>
2023-03-30 11:57:51 -0400

    potential fix

--- server/api/openai/generate-board.post.ts
+++ server/api/openai/generate-board.post.ts
@@ -8,21 +8,21 @@ export default defineEventHandler(async (event) => {
     const itemId = body.projectDescription
     console.log("projectDescription", projectDescription)
     console.log("itemId", itemId)
-    if (!projectDescription) {
-      event.node.res.statusCode = 400
-      event.node.res.end()
-    }
-    if (!itemId) {
-      event.node.res.statusCode = 400
-      event.node.res.end()
-    }
+    // if (!projectDescription.isEmpty) {
+    //   event.node.res.statusCode = 400
+    //   return null
+    // }
+    // if (!itemId) {
+    //   event.node.res.statusCode = 400
+    //   return null
+    // }
     const response = await openaiApi(
       `I want to start a new project. I'm going to make a new Trello board with tasks to complete my project. Generate a name for my project. Take in the description of my project and generate a JSON that matches the following format:\n\ntype Project {\n  projectName: string,\n  ticketList: List of Ticket,\n}\ntype Ticket {\n  title: string,\n  scope: string,\n  acceptance criteria: List of string // one string per criteria\n  helpfulResources: List of string\n}\n\nRespond in json format (with properties wrapped in double quotes.\n\nPlease help me see my project to the finish line! Here's a description of my project:\n\n ${projectDescription}.`
     )
-    const projectBoardData = getProjectBoardFromRes(response, itemId)
+    const projectBoardData = await getProjectBoardFromRes(response, itemId)
     if (!projectBoardData) {
       event.node.res.statusCode = 400
-      event.node.res.end()
+      return null
     }
     console.log("****projectBoardData****")
     console.dir(projectBoardData)
@@ -30,6 +30,6 @@ export default defineEventHandler(async (event) => {
   } catch (e) {
     console.error(e)
     event.node.res.statusCode = 500
-    event.node.res.end()
+    return null
   }
 })
