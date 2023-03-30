3098218efcd9f1cf58b8bd5a61f48c0061d12e36
Jimmy McBride <jimmy@ravebizz.com>
2023-03-30 13:17:34 -0400

    removed useless timeout param

--- server/utils/openai/index.ts
+++ server/utils/openai/index.ts
@@ -7,18 +7,15 @@ const configuration = new Configuration({
 export const openai = new OpenAIApi(configuration)
 
 export const openaiApi = async (prompt: string) => {
-  return await openai.createCompletion(
-    {
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0.7,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    },
-    { timeout: 90000 }
-  )
+  return await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.7,
+    max_tokens: 3000,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
 }
 
 function tokenize(text: string): string[] {
