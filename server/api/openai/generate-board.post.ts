289f20ffdd351d6e7b2a0e025773dd6836f4dece
Jimmy McBride <jimmy@ravebizz.com>
2023-03-30 11:34:18 -0400

    potential fix

--- server/api/openai/generate-board.post.ts
+++ server/api/openai/generate-board.post.ts
@@ -5,11 +5,13 @@ export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event)
     const projectDescription = body.projectDescription
+    const itemId = body.projectDescription
+    console.log("projectDescription", projectDescription)
+    console.log("itemId", itemId)
     if (!projectDescription) {
       event.node.res.statusCode = 400
       event.node.res.end()
     }
-    const itemId = body.itemId
     if (!itemId) {
       event.node.res.statusCode = 400
       event.node.res.end()
@@ -17,15 +19,17 @@ export default defineEventHandler(async (event) => {
     const response = await openaiApi(
       `I want to start a new project. I'm going to make a new Trello board with tasks to complete my project. Generate a name for my project. Take in the description of my project and generate a JSON that matches the following format:\n\ntype Project {\n  projectName: string,\n  ticketList: List of Ticket,\n}\ntype Ticket {\n  title: string,\n  scope: string,\n  acceptance criteria: List of string // one string per criteria\n  helpfulResources: List of string\n}\n\nRespond in json format (with properties wrapped in double quotes.\n\nPlease help me see my project to the finish line! Here's a description of my project:\n\n ${projectDescription}.`
     )
-    const projectBoardData = getProjectBoardFromRes(response, body.itemId)
+    const projectBoardData = getProjectBoardFromRes(response, itemId)
     if (!projectBoardData) {
       event.node.res.statusCode = 400
       event.node.res.end()
     }
+    console.log("****projectBoardData****")
+    console.dir(projectBoardData)
     return projectBoardData
   } catch (e) {
     console.error(e)
-    event.node.res.statusCode = 400
+    event.node.res.statusCode = 500
     event.node.res.end()
   }
 })
